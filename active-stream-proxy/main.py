35c2a98835dd83c85f2d8a391f3de6345f1c722a
Micha Albert <micha@2231puppy.tech>
2024-09-10 13:32:34 -0400

    kinda working

--- active-stream-proxy/main.py
+++ active-stream-proxy/main.py
@@ -2,9 +2,9 @@ import subprocess
 import time
 import requests
 
-time.sleep(5)
+time.sleep(10)
 
-active_stream = requests.get("http://backend:8000/api/v1/active_stream").text
+active_stream = requests.get("http://backend:8000/api/v1/active_stream").text.replace('"', '')
 print(active_stream)
 
 old_active_stream = active_stream
@@ -12,19 +12,18 @@ old_active_stream = active_stream
 proc = None
 
 if active_stream != "":
-    proc = subprocess.Popen(["/bin/ffmpeg", "-re", "-i", f"rtmp://mediamtx:1935/{active_stream}", "-c:a copy", "rtmp://live-stream:1936/active-input"])
+    proc = subprocess.Popen(["ffmpeg", "-re", "-i", f"rtmp://mediamtx:1935/{active_stream}", "-c:a", "libmp3lame", "-f", "flv", "rtmp://host.containers.internal:1936/active-input"])
 else:
-    proc = subprocess.Popen(["/bin/ffmpeg", "-i", "anullsrc", "-c:a copy", "rtmp://live-stream:1936/active-input"])
-
+    proc = subprocess.Popen(["ffmpeg", "-f", "lavfi", "-i", "anullsrc", "-c:a", "libmp3lame", "-f", "flv", "rtmp://host.containers.internal:1936/active-input"])
 
 while True:
     time.sleep(3)
-    active_stream = requests.get("http://backend:8000/api/v1/active_stream").text
+    active_stream = requests.get("http://backend:8000/api/v1/active_stream").text.replace('""', '')
     if old_active_stream is not active_stream:
         if proc:
             proc.terminate()
         if active_stream != "":
-            proc = subprocess.Popen(["/bin/ffmpeg", "-re", "-i", f"rtmp://mediamtx:1935/{active_stream}", "-c:a copy", "rtmp://live-stream:1936/active-input"])
+            proc = subprocess.Popen(["ffmpeg", "-re", "-i", f"rtmp://mediamtx:1935/{active_stream}", "-c:a", "libmp3lame", "-f", "flv", "rtmp://host.containers.internal:1936/active-input"])
         else:
-            proc = subprocess.Popen(["/bin/ffmpeg", "-i", "anullsrc", "-c:a copy", "rtmp://live-stream:1936/active-input"])
+            proc = subprocess.Popen(["ffmpeg", "-f", "lavfi", "-i", "anullsrc", "-c:a", "libmp3lame", "-f", "flv", "rtmp://host.containers.internal:1936/active-input"])
     old_active_stream = active_stream
--- live-stream/run.sh
+++ live-stream/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-gst-launch-1.0 -e wpesrc location="https://en.wikipedia.org/wiki/Main_Page" \
+sleep 1
+
+LIBGL_ALWAYS_SOFTWARE=true gst-launch-1.0 -e wpesrc location="http://web-frontend:4173/" \
+          ! queue \
           ! videoconvert ! videoscale ! videorate \
-          ! "video/x-raw, format=BGRA, width=854, height=480, framerate=30/1" \
+          ! "video/x-raw, format=BGRA, width=1920, height=1080, framerate=30/1" \
           ! videoconvert \
           ! x264enc speed-preset=1 \
-          ! filesink location=/dev/stdout | ffmpeg -re -y -i - -listen 1 -i rtmp://0.0.0.0:1936/active-input -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 -t 10 -f flv rtmp://x.rtmp.youtube.com/live2/no-way-am-i-doing-that-again
+          ! filesink location=/dev/stdout | ffmpeg -re -y -i - -listen 1 -i rtmp://0.0.0.0:1936/active-input -c:v copy -c:a libmp3lame -map 0:v:0 -map 1:a:0 -g 90 -framerate 30 -movflags faststart -bufsize 14000k -f flv rtmp://x.rtmp.youtube.com/live2/$YT_STREAM_KEY
+
+
+
