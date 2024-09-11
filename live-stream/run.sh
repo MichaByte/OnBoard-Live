1c91c56d308dcd7fa43319e9b8b42b9c0cf93114
Micha Albert <micha@2231puppy.tech>
2024-09-11 16:10:23 -0400

    wip

--- live-stream/run.sh
+++ live-stream/run.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-sleep 1
+export CHROMIUM_FLAGS="--disable-software-rasterizer --disable-dev-shm-usage"
+dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
+export LIBGL_ALWAYS_INDIRECT=1
 
-LIBGL_ALWAYS_SOFTWARE=true gst-launch-1.0 -e wpesrc location="http://web-frontend:4173/" \
-          ! videoconvert ! videoscale ! videorate \
-          ! "video/x-raw, format=NV12, width=1920, height=1080, framerate=30/1" \
-          ! filesink location=/dev/stdout | ffmpeg -re -y -f rawvideo -pixel_format nv12 -video_size 1920x1080 -framerate 30 -i - -listen 1 -i rtmp://0.0.0.0:1936/active-input -filter_complex "[0:v][0:v]overlay=-50:0[bg]; [bg][0:v]overlay=-50-W,format=nv12[out]" -map "[out]" -c:v libx264 -x264-params keyint=60 -preset ultrafast -b:v 6800k -c:a copy -map 1:a:0 -movflags faststart -f flv -pix_fmt nv12 rtmp://x.rtmp.youtube.com/live2/$YT_STREAM_KEY
+bash -c "sleep 5 && DISPLAY=:99 ffmpeg -f x11grab -r 30 -s 1920x1080 -draw_mouse 0 -i :99.0 -f alsa -ac 2 -i hw:0 -vcodec libx264 -preset medium -b:v 7000k -framerate 30 -g 2 -pix_fmt yuv420p -acodec aac -f flv rtmp://x.rtmp.youtube.com/live2/$YT_STREAM_KEY" &
+
+DISPLAY=:99 xvfb-run \
+  --server-num 99 \
+  -s "-nocursor -ac -screen 0 1920x1080x24" \
+  dbus-launch chromium \
+    --temp-profile \
+    --window-size=1920,1080 \
+    --disable-gpu \
+    --window-position=0,0 \
+    --no-sandbox \
+    --hide-scrollbars \
+    --disable-setuid-sandbox \
+    --app=http://web-frontend:4173
