ee467d25651966e798709d730ef89fcc8ffbccf5
Micha Albert <micha@2231puppy.tech>
2024-09-12 15:41:34 -0400

    this should do it

--- live-stream/user_run.sh
+++ live-stream/user_run.sh
@@ -9,7 +9,7 @@ pacmd update-sink-proplist VirtSink device.description=VirtSink
 export CHROMIUM_FLAGS="--disable-software-rasterizer --disable-dev-shm-usage"
 export LIBGL_ALWAYS_INDIRECT=1
 
-bash -c "sleep 5 && DISPLAY=:99 ffmpeg -f x11grab -r 30 -s 2560x1440 -draw_mouse 0 -i :99.0 -f pulse -ac 2 -i default -vcodec libx264 -preset medium -b:v 15000k -framerate 30 -g 2 -pix_fmt yuv420p -acodec aac -f flv rtmp://x.rtmp.youtube.com/live2/$(cat /home/stream/key.txt)" &
+bash -c "sleep 5 && DISPLAY=:99 ffmpeg -f x11grab -r 30 -s 2560x1440 -draw_mouse 0 -i :99.0 -f pulse -ac 2 -i default -vcodec libx264 -preset faster -b:v 15000k -framerate 30 -g 2 -pix_fmt yuv420p -acodec aac -f flv rtmp://x.rtmp.youtube.com/live2/$(cat /home/stream/key.txt)" &
 
 DISPLAY=:99 xvfb-run \
     --server-num 99 \
