ba295dda7eef46aa01d233e4fb6e1522b0503d79
Micha Albert <micha@2231puppy.tech>
2024-09-12 15:54:20 -0400

    modify keyframe interval

--- live-stream/user_run.sh
+++ live-stream/user_run.sh
@@ -9,7 +9,7 @@ pacmd update-sink-proplist VirtSink device.description=VirtSink
 export CHROMIUM_FLAGS="--disable-software-rasterizer --disable-dev-shm-usage"
 export LIBGL_ALWAYS_INDIRECT=1
 
-bash -c "sleep 5 && DISPLAY=:99 ffmpeg -f x11grab -r 60 -s 1920x1080 -draw_mouse 0 -i :99.0 -f pulse -ac 2 -i default -vcodec libx264 -preset medium -b:v 7000k -framerate 60 -g 2 -pix_fmt yuv420p -acodec aac -f flv rtmp://x.rtmp.youtube.com/live2/$(cat /home/stream/key.txt)" &
+bash -c "sleep 5 && DISPLAY=:99 ffmpeg -f x11grab -r 60 -s 1920x1080 -draw_mouse 0 -i :99.0 -f pulse -ac 2 -i default -vcodec libx264 -preset medium -b:v 7000k -framerate 60 -g 120 -pix_fmt yuv420p -acodec aac -f flv rtmp://x.rtmp.youtube.com/live2/$(cat /home/stream/key.txt)" &
 
 DISPLAY=:99 xvfb-run \
     --server-num 99 \
