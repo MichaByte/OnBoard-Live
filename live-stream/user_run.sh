8343f69bb8be47e4b6e9a7d8a45011a7ef525526
Micha Albert <micha@2231puppy.tech>
2024-09-17 18:45:46 -0400

    make this not crash as much hopefully

--- live-stream/user_run.sh
+++ live-stream/user_run.sh
@@ -9,9 +9,8 @@ pacmd update-sink-proplist VirtSink device.description=VirtSink
 export CHROMIUM_FLAGS="--disable-software-rasterizer --disable-dev-shm-usage"
 export LIBGL_ALWAYS_INDIRECT=1
 
-bash -c "sleep 5 && DISPLAY=:99 ffmpeg -f x11grab -r 60 -s 1920x1080 -draw_mouse 0 -i :99.0 -f pulse -ac 2 -i default -vcodec libx264 -preset medium -b:v 7000k -framerate 60 -g 2 -pix_fmt yuv420p -acodec aac -f flv rtmp://x.rtmp.youtube.com/live2/$(cat /home/stream/key.txt)" &
 
-DISPLAY=:99 xvfb-run \
+bash -c "DISPLAY=:99 xvfb-run \
     --server-num 99 \
     -s "-nocursor -ac -screen 0 1920x1080x24" \
     dbus-launch chromium \
@@ -21,4 +20,6 @@ DISPLAY=:99 xvfb-run \
     --window-position=0,0 \
     --hide-scrollbars \
     --autoplay-policy=no-user-gesture-required \
-    --app=http://localhost:4173
+    --app=http://localhost:4173" & disown
+
+bash -c "sleep 3 && DISPLAY=:99 ffmpeg -f x11grab -r 60 -s 1920x1080 -draw_mouse 0 -i :99.0 -f pulse -ac 2 -i default -vcodec libx264 -preset medium -b:v 7000k -framerate 60 -g 2 -pix_fmt yuv420p -acodec aac -f flv rtmp://x.rtmp.youtube.com/live2/$(cat /home/stream/key.txt)"
