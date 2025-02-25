4b51070146c21a28ed487a9137708428ab50cd8c
Micha Albert <micha@2231puppy.tech>
2025-02-25 17:47:01 -0500

    tweak ffmpeg settings

--- live-stream/user_run.sh
+++ live-stream/user_run.sh
@@ -24,7 +24,7 @@ bash -c "DISPLAY=:99 xvfb-run \
 
 sleep 3
 
-until bash -c "DISPLAY=:99 ffmpeg -xerror -f x11grab -r 30 -s 1920x1080 -draw_mouse 0 -i :99.0 -f pulse -ac 2 -i default -vcodec libx264 -preset faster -b:v 6000k -bufsize 12000k -framerate 30 -g 60 -pix_fmt yuv420p -acodec aac -f flv -flvflags no_duration_filesize rtmp://x.rtmp.youtube.com/live2/$(cat /home/stream/key.txt)"; do
+until bash -c "DISPLAY=:99 ffmpeg -xerror -f x11grab -r 30 -s 1920x1080 -draw_mouse 0 -i :99.0 -f pulse -ac 2 -i default -vcodec libx264 -preset veryfast -tune zerolatency -b:v 6000k -bufsize 12000k -framerate 30 -g 60 -pix_fmt yuv420p -acodec aac -f flv -flvflags no_duration_filesize rtmp://x.rtmp.youtube.com/live2/$(cat /home/stream/key.txt)"; do
         echo "Restarting FFmpeg..."
         sleep 1
 done
